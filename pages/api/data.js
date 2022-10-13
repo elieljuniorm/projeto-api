c74800fe0f0f976391a55cbd291f9ac3b7fe79df
Eliel Maia <elieljuniorm@gmail.com>
2022-10-13 13:20:02 -0300

    aviso para gerenciar cache

--- pages/api/data.js
+++ pages/api/data.js
@@ -3,7 +3,8 @@ function data(req, res) {
   const envNome = process.env.NOME_AMBIENTE;
 
   res.status(200).json({ nome: envNome })
-  
-}
 
+}
+/* Codigo para gerenciamento de cache para consumo de banco.
+ response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate'); */
 export default data
