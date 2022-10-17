9ace1e04163c2b972f287c4f2bf2a994e2c46aab
Eliel Maia <elieljuniorm@gmail.com>
2022-10-17 17:25:43 -0300

    finzalindo consumo do banco

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,15 +1,16 @@
 import connect from '../../model/database';
 
 
-export default async function handler(req, res){
-    
-    const {db} = await connect()
+export default async function handler(req, res) {
+
+    const { db } = await connect()
 
     const user = {
         name: req.body.name,
         password: req.body.password
     }
 
+    //retorna o dado do banco com base na pesquisa da const user
     const response = await db.collection("users").findOne(user)
 
     res.status(200).json(response)
