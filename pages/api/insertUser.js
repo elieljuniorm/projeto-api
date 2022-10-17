436ed5fe7cac3475679466a793bebc591aa036bf
Eliel Maia <elieljuniorm@gmail.com>
2022-10-17 18:21:34 -0300

    insert no banco com novos usuarios

---
+++ pages/api/insertUser.js
@@ -0,0 +1,17 @@
+import connect from '../../model/database';
+
+
+export default async function handler(req, res) {
+
+    const { db } = await connect()
+
+    const user = {
+        name: req.body.name,
+        password: req.body.password
+    }
+
+    //retorna o dado do banco com base na pesquisa da const user
+    const response = await db.collection("users").insertOne(user)
+
+    res.status(200).json({msg: "Usuário cadastrado com sucesso!"})
+}
