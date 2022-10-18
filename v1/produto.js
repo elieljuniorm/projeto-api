1fc98a7be85b9902d6212f9961ddd01e725daf5a
Eliel Maia <elieljuniorm@gmail.com>
2022-10-18 18:49:59 -0300

    colocando dados

--- v1/produto.js
+++ v1/produto.js
@@ -1,18 +1,107 @@
 export const produtos = [
     {
         "nome": "Celular",
-        "preco": "1000"
+        "preco": "2599,90",
+        "img": "https://images.pexels.com/photos/11120516/pexels-photo-11120516.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         "nome": "Notebook",
-        "preco": "2000"
+        "preco": "4699,90",
+        "img": "https://images.pexels.com/photos/11982694/pexels-photo-11982694.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         "nome": "Câmera",
-        "preco": "3000"
+        "preco": "5190,99",
+        "img": "https://images.pexels.com/photos/165978/pexels-photo-165978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Teclado",
+        "preco": "259,90",
+        "img": "https://images.pexels.com/photos/14063089/pexels-photo-14063089.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Drone",
+        "preco": "10650,99",
+        "img": "https://images.pexels.com/photos/6624608/pexels-photo-6624608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Fone de Ouvido",
+        "preco": "109,90",
+        "img": "https://images.pexels.com/photos/3394651/pexels-photo-3394651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Mouse",
+        "preco": "319,90",
+        "img": "https://images.pexels.com/photos/9058886/pexels-photo-9058886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Óculos de Sol",
+        "preco": "199,90",
+        "img": "https://images.pexels.com/photos/10260111/pexels-photo-10260111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Sofá",
+        "preco": "2109,90",
+        "img": "https://images.pexels.com/photos/11112731/pexels-photo-11112731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Microfone",
+        "preco": "990,99",
+        "img": "https://images.pexels.com/photos/352505/pexels-photo-352505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Tennis",
+        "preco": "99,90",
+        "img": "https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Relógio",
+        "preco": "2990,99",
+        "img": "https://images.pexels.com/photos/12564670/pexels-photo-12564670.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Video Game Portátil",
+        "preco": "3299,90",
+        "img": "https://images.pexels.com/photos/14005916/pexels-photo-14005916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Controle de PS5",
+        "preco": "399,90",
+        "img": "https://images.pexels.com/photos/11591876/pexels-photo-11591876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Suporte de Celular",
+        "preco": "89,90",
+        "img": "https://images.pexels.com/photos/12953565/pexels-photo-12953565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Bolsa",
+        "preco": "210,50",
+        "img": "https://images.pexels.com/photos/3808229/pexels-photo-3808229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Anel de Brilhante",
+        "preco": "489,90",
+        "img": "https://images.pexels.com/photos/10475789/pexels-photo-10475789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         "nome": "Cadeira",
-        "preco": "400"
+        "preco": "109,50",
+        "img": "https://images.pexels.com/photos/2762247/pexels-photo-2762247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
+    {
+        "nome": "Boneco Decorativo",
+        "preco": "79,90",
+        "img": "https://images.pexels.com/photos/5558237/pexels-photo-5558237.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Isqueiro",
+        "preco": "19,90",
+        "img": "https://images.pexels.com/photos/6580543/pexels-photo-6580543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "nome": "Armário Decorativo",
+        "preco": "399,90",
+        "img": "https://images.pexels.com/photos/11112746/pexels-photo-11112746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    }
 ]
