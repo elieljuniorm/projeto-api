23611aa2bde1969b0dcf305384cbc91a35946694
Eliel Maia <elieljuniorm@gmail.com>
2022-11-08 18:58:25 -0300

    identando

--- pages/api/login/login.js
+++ pages/api/login/login.js
@@ -1,6 +1,6 @@
 import login from "../../../model/userCRUD"
 
-export default async function userLogin(req, res){
+export default async function userLogin(req, res) {
 
     const user = {
         name: req.body.name,
@@ -8,6 +8,6 @@ export default async function userLogin(req, res){
     }
 
     const response = await login(user.name, user.password)
- 
+
     res.status(200).json(response)
 }
