e8634fa645980951bf5b2c129bcdf7c002049140
Eliel Maia <elieljuniorm@gmail.com>
2022-11-08 20:12:47 -0300

    index base

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div style={{textAlign: "center"}}>
+        <h1>API Eliel Maia - Links úteis</h1>
+        <a href="https://api.elielmaia.dev/api/data" style={{fontSize: "2em", textDecoration: "none"}}>Rota Pública - Produtos</a>
+    </div>
+  )
+}
+
+export default index
