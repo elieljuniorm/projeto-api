f8f558617ce7ae09e254a4d59269ed13d768c595
Eliel Maia <elieljuniorm@gmail.com>
2022-11-08 19:52:25 -0300

    validando metodo

--- pages/api/login/login.js
+++ pages/api/login/login.js
@@ -2,12 +2,17 @@ import login from "../../../model/userCRUD"
 
 export default async function userLogin(req, res) {
 
-    const user = {
-        name: req.body.name,
-        password: req.body.password,
+    if (req.method === 'GET') {
+        const user = {
+            name: req.body.name,
+            password: req.body.password,
+        }
+
+        const response = await login(user.name, user.password)
+
+        res.status(200).json(response)
+
+    } else {
+        res.status(405).json({ msg: "Método não suportado!" })
     }
-
-    const response = await login(user.name, user.password)
-
-    res.status(200).json(response)
 }
