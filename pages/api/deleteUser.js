dad720b5998c287b74002f7c6b1fee44f7eb2cff
Eliel Maia <elieljuniorm@gmail.com>
2022-11-19 16:17:18 -0300

    Update deleteUser.js

--- pages/api/deleteUser.js
+++ pages/api/deleteUser.js
@@ -7,16 +7,29 @@ export default async function handler(req, res) {
 
         const { db } = await connect()
 
-        const user = {
-            id: req.body._id
+        const Users = db.collection("users")
+
+        const mongo = require('mongodb')
+
+        const user = { "_id" : new mongo.ObjectId(req.body.id) }
+        
+        //retorna o dado do banco com base na pesquisa da const user
+        try {
+            const response = await Users.deleteOne(user)
+            if (response.deletedCount == 0) {
+                response.message = "Não foi possível excluir o usuário."
+                res.status(500).json(response)
+            } else {
+                response.message = "Usuário excluído com sucesso!"
+                res.status(200).json(response)
+            }
+        } catch (e) {
+            console.log(e);
         }
 
-        //retorna o dado do banco com base na pesquisa da const user
-        const response = await db.collection("users").findOneAndRemove(user)
-
-        res.status(200).json({msg: "Cadastro excluido"})
-
-    } else {
-        res.status(405).json(response)
+        
+        
+    } else{
+        res.status(405).json({ msg: "Método não suportado!" })
     }
 }
